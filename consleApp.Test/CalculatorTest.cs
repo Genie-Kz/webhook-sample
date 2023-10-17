e510cc089050304a8e85e5da134e7b9cd6c545d0
Genie-Kz <126337294+Genie-Kz@users.noreply.github.com>
2023-10-18 00:57:38 +0900

    add: AddTest

---
+++ consleApp.Test/CalculatorTest.cs
@@ -0,0 +1,17 @@
+consleApp.Test;
+
+public class CalculatorTest
+{
+    [Fact]
+    public void AddTest()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Add(1, 2);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
---
+++ consoleApp/Calculator.cs
@@ -0,0 +1,7 @@
+public class Calulator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
